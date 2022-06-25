4746414d571590b3949d6366e3c22825e89c8379
unknown <joaovneres@outlook.com>
2022-06-24 22:03:41 -0300

    Update

--- farmacia/src/main/java/com/generation/farmacia/model/Produto.java
+++ farmacia/src/main/java/com/generation/farmacia/model/Produto.java
@@ -2,6 +2,7 @@ package com.generation.farmacia.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
@@ -19,7 +20,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Produto {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idProduto;
 	
 	@NotBlank
